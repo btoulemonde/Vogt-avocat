2653fe431ab564e55020daa90e38dd219732c088
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2020-05-18 18:58:45 +0200

    component en place et routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AccueilComponent } from './accueil/accueil.component';
+import { FooterComponent } from './footer/footer.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import {CompetencesComponent} from './competences/competences.component';
+
+
+const routes: Routes = [
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'presentation', component: PresentationComponent},
+  {path: 'domaineCompétences', component: CompetencesComponent},
+  {path: '', redirectTo: 'accueil', pathMatch: 'full'}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContactComponent,
+    AccueilComponent,
+    FooterComponent,
+    PresentationComponent,
+    CompetencesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
